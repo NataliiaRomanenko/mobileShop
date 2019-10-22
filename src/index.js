4b2481287bd1eea8bca2e98cb40c1a66ccd841ed
Nataly Romanenko <nataliia.romanenko@gmail.com>
2019-10-22 15:40:42 +0300

    structure of project. Fetch phones

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+import './main.css'
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import {applyMiddleware, createStore} from 'redux';
+import {createBrowserHistory} from 'history';
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from 'redux-thunk';
+import {routerMiddleware, ConnectedRouter} from 'connected-react-router';
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import createRootReducer from 'reducers';
+import Layout from 'containers/layout'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const history = createBrowserHistory()
+const middlewares= [thunk, routerMiddleware(history)];
+const store = createStore(
+    createRootReducer(history),
+    composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+ReactDom.render(
+    <Provider store = {store}>
+        <ConnectedRouter history = {history}>
+            <Layout/>
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+)
+
